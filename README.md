f68c0d5464097b5eb6fa1084d84c52df01990b04
Kevin Lai <kevin.km.lai@gmail.com>
2019-08-23 20:36:21 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+30 DAYS of REACT CHALLENGE
+
+Motivation:
+To build a calculator that has basic functionality
+
+1. Calculate the result of our expression
+2. Clear our output, this is triggered when “C” is pressed.
+3. Clear the last character that was pressed. triggered on “CE”.
---
+++ src/components/Keypad.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// Send the click event to the parent
+// when that particular button is clicked,
+// we will call this.props.OnClick function on every button click
+// and pass e.target.name as an argument.
+
+class Keypad extends Component {
+
+    render() {
+        const buttons = ["(","CE", ")", "C", "1","2","3","+","4","5","6","-","7", "8", "9", "*", ".","0","=","/" ]
+        return (
+            <div className="button">
+
+                {buttons.map((num) => <button onClick={e => this.props.onClick(e.target.name)}>{num}</button>)}
+            </div>
+         );
+    }
+}
+
+export default Keypad;
